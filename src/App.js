466efb792f2dab8851fafe2b9969f923c617f9a6
chad97 <chad97@126.com>
2019-09-16 16:54:25 +0800

    submit first

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Ruoduan of firs electron-react-app</p>
+        <a href="https://ruoduan.cn">my-Blog</a>
       </header>
     </div>
   );
